b40706d77c850c241e166a810c4794e3d9e32979
Valerie Schweitzer <vmschweitzer@gmail.com>
2022-08-31 17:09:39 +0200

    updated questionaire

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: :find_animals
 
   def home
   end
 
   def find_animals
-
   end
 
   def dashboard
