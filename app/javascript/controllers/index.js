9d48d60af055c811ae82dd7fdc6f3a498f8e1378
Xinchu Gao xinchu-gao@outlook.com <xinchu-gao@outlook.com>
2022-09-01 15:20:39 +0200

    update sth

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,7 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+const remote = false;
+
+const str = remote === true ? 'Yes' : 'No';
