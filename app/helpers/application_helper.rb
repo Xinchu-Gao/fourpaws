b9befbf4e481a8b716cd07899b23501460e575b5
Xinchu Gao xinchu-gao@outlook.com <xinchu-gao@outlook.com>
2022-09-08 09:58:27 +0200

    new og image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'Fourpaws',
+      title: 'Fourpaws',
+      reverse: true,
+      separator: '|',
+      description: 'Adopt, do not shop!',
+      keywords: 'animals, organize, lifestyle',
+      canonical: request.original_url,
+      noindex: !Rails.env.production?,
+      icon: [
+        { href: image_url('fackbook_cat.png'), rel: 'touch-icon', type: 'image/png' },
+      ],
+      og: {
+        site_name: 'Fourpaws',
+        title: 'Fourpaws',
+        description: 'Adopt, do not shop!',
+        type: 'website',
+        url: request.original_url,
+        image: image_url('facebook_cat.png')
+      }
+    }
+  end
 end
